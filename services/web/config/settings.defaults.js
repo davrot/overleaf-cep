deb8b24ec91ce7dee4cc3f0de11ce76b952a80d8
yu-i-i <kla9555@gmail.com>
2026-03-14 02:33:19 +0100

    Git Bridge: Add git integration

--- services/web/config/settings.defaults.js
+++ services/web/config/settings.defaults.js
@@ -1067,7 +1067,12 @@ module.exports = {
     referenceLinkingWidgets: [],
     importProjectFromGithubModalWrapper: [],
     importProjectFromGithubMenu: [],
-    editorLeftMenuSync: [],
+    editorLeftMenuSync: [
+      Path.resolve(
+        __dirname,
+        '../modules/git-bridge/frontend/js/card/components/git-modal.tsx'
+      ),
+    ],
     editorLeftMenuManageTemplate: [
       Path.resolve(
         __dirname,
@@ -1081,7 +1086,12 @@ module.exports = {
       ),
     ],
     insertMenuSections: [],
-    oauth2Server: [],
+    oauth2Server: [
+      Path.resolve(
+        __dirname,
+        '../modules/git-bridge/frontend/js/widget/components/git-integration-widget.tsx'
+      )
+    ],
     managedGroupSubscriptionEnrollmentNotification: [],
     managedGroupEnrollmentInvite: [],
     ssoCertificateInfo: [],
@@ -1118,7 +1128,12 @@ module.exports = {
         '../modules/full-project-search/frontend/js/components/full-project-search.tsx'
       ),
     ],
-    integrationPanelComponents: [],
+    integrationPanelComponents: [
+      Path.resolve(
+        __dirname,
+        '../modules/git-bridge/frontend/js/card/components/git-integration-card.tsx'
+      ),
+    ],
     referenceSearchSetting: [],
     settingsModalEditorTabSections: [],
     settingsModalSpellcheckSections: [],
@@ -1142,6 +1157,7 @@ module.exports = {
     'authentication/oidc',
     'admin-tools', // import after authentication
     'template-gallery',
+    'git-bridge',
   ],
   viewIncludes: {},
 
@@ -1155,7 +1171,6 @@ module.exports = {
       'app/views/project/ide-react': [`img-src 'self' data: blob:`],
     },
   },
-
   unsupportedBrowsers: {
     ie: '<=11',
     safari: '<15',
